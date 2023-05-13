36f8a0907207a286ace75dba94bf7ee177f8ec0a
f0relsket9 <inimuftiali@gmail.com>
2023-05-13 20:24:39 +0700

    init

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,18 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Borgir's Website";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const Experiences = [
+  {
+    Title: "My First Internship",
+    Description: "I jadi babu",
+    Image: "",
+  },
+  {
+    Title: "My Second Internship",
+    Description: "I jadi babu (masih)",
+    Image: "",
+  },
+];
